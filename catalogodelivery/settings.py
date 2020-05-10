ccc2d0757902af969d5efb2c6fd3e98350316cb5
guedesemerson <guedes.emerson@hotmail.com>
2020-05-10 15:56:49 -0300

    ajustando7

--- catalogodelivery/settings.py
+++ catalogodelivery/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap_modal_forms',
     'estabelecimento',
     'categoria',
     'core',
